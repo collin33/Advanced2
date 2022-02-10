b2c15e5583c5f10b5149c7418adb856a053f815f
Collin Meinders <colacollin@hotmail.com>
2022-02-10 12:41:11 +0100

    Klein begin
    - Scripts zijn nu in een script folder
    - Script voor GameObject en Transform zijn aangemaakt
    
    ! Gameobject is nog niet klaar
    ! moet nog veel logica worden toegevoegd. Denken in lastig. Commits worden vooral uitgevoerd zodat ik geen progressie kwijtraak bij toekomstige fouten.

---
+++ Advanced2/Scripts/GameObject.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+public class GameObject
+{
+    //naam van het gameobject
+    //string name;
+
+
+    //game
+
+    //spritebatch
+    //public virtual void spriterenderer(spritebatch, transform)
+    //{
+
+    //}
+}
---
+++ Advanced2/Scripts/Transform.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+public class Transform : GameObject
+{
+    Vector2 Position;
+    float Rotation;
+    Vector2 Scale;
+}
